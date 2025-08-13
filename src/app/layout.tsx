3b83f8b64ffcbcf9c5d42b2d83e764eb70364ae2
李旭彬 <li1xu1bin@gmail.com>
2025-08-13 03:56:49 +0000

    网站标题改成NAV1024工具站，ico图标用favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import './globals.css';
 import {Toaster} from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Navigator',
+  title: 'NAV1024工具站',
   description: 'Your personal navigation page',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
