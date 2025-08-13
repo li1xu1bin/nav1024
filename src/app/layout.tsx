6f3bfbe96dadf4014f7ca357d6db50c9a1ca7d40
lixubin <lixubin@gmail.com>
2025-08-13 19:52:15 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
-import {Toaster} from '@/components/ui/toaster';
+import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
   title: 'NAV1024工具站',
-  description: 'Your personal navigation page',
+  description: '个人AI工具站',
   icons: {
     icon: '/favicon.ico',
   },
