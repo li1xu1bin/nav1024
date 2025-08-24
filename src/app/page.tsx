119819e770842c548487e3c37e2767dffd1162b0
lixubin <lixubin@gmail.com>
2025-08-25 00:04:03 +0800

    debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from 'react';
 import {NavigatorClient} from '@/components/navigator-client';
 import {websites, categories} from '@/lib/data';
 
 export default function Home() {
-  return <NavigatorClient categories={categories} websites={websites} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavigatorClient categories={categories} websites={websites} />
+    </Suspense>
+  );
 }
